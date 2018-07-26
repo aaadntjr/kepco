993ee8c68225f83a7d1911a44be5097aa284da31
Choi woo seok <38950467+aaadntjr@users.noreply.github.com>
2018-07-26 18:16:04 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    
+    //로그인 메뉴 열고닫기
+    $(".login_open").click(function(){
+        $(this).toggleClass("login_close");
+        $(".login_menu").slideToggle();
+        return false;
+    });
+    
+    //서브메뉴 열기    
+    $("ul.menu").mouseover(function(){
+       $(".sub_menu").stop().slideDown(300);
+        return false;
+    });
+    
+    //서브메뉴 닫기    
+    $("ul.menu").mouseleave(function(){
+       $(".sub_menu").stop().slideUp(300);
+        return false;
+    });
+    
+    //메인 타이틀
+    $(".section01_title li").addClass("section01_active");
+    
+    $(window).scroll(function(){
+        //그래프 돌리기
+        var top = $(this).scrollTop();
+        var view_point = $(".section03_title").offset().top;
+//        console.log(top,view_point);
+        
+        var full_kw = 1000;
+        var full_deg = -45;
+        var now_kw = 1000; // 현재 전력 입력
+        var now_deg = full_deg - (((full_kw - now_kw) / 100)*18);
+        
+        console.log(now_deg);
+        if(top>=view_point) {
+            $(".graph_circle").css({"transform":"rotate("+now_deg+"deg)"});
+            return false;
+        }
+    
+    });/*스크롤펑션*/
+    
+    
+
+    //패밀리 닫기
+    $(".family_close").click(function(){
+       $(".family").slideUp();
+        $(".family_open").show();
+        return false;
+    });
+    
+        //패밀리 열기
+    $(".family_open").click(function(){
+       $(".family").slideDown();
+        $(this).hide();
+        return false;
+    });
+    $(".family_site").click(function(){
+       $(".family").slideDown();
+        $(".family_open").hide();
+        return false;
+    });
+    
+    
+    
+    
+    
+    
+});/*레디 펑션!*/
